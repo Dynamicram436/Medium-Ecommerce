f4a5f800cd3f564d51498633415496092015880b
Dynamicram436 <115802967+Dynamicram436@users.noreply.github.com>
2025-09-26 12:11:32 +0530

    Update products.jsx

--- ReactTasks/frontend/src/Products/products.jsx
+++ ReactTasks/frontend/src/Products/products.jsx
@@ -21,10 +21,9 @@ const Products = () => {
     const fetchProducts = async () => {
       const res = await axios.get("https://api.escuelajs.co/api/v1/products");
       setProducts(res.data);
-    };
+    };  
     fetchProducts();
   }, []);
-
   return (
     <div className="max-w-7xl mx-auto px-4 py-8">
       {/* <h1 className="text-3xl font-bold mb-8 text-center text-gray-800">
