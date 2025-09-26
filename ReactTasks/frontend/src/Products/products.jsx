0bbf01c90e1349c5c63dce3969c094e50c9383a8
Dynamicram436 <115802967+Dynamicram436@users.noreply.github.com>
2025-09-26 11:49:40 +0530

    added 2xl width for PC

--- ReactTasks/frontend/src/Products/products.jsx
+++ ReactTasks/frontend/src/Products/products.jsx
@@ -31,7 +31,7 @@ const Products = () => {
         Welcome {JSON.parse(localStorage.getItem("userData")).username} to the
         Products Page
       </h1> */}
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
         {products.map(({ id, title, price, slug, images, description }) => (
           <div
             key={id}
